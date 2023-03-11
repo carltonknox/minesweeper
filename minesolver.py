f89f3c0fdf407cc12df3ff7751f0317c3ebc5d2a
Carlton Knox <cknox@bu.edu>
2023-03-11 15:28:32 -0500

    added advanced algorithm

--- minesolver.py
+++ minesolver.py
@@ -11,15 +11,14 @@ while(not gameover):
     gameover = not any("?" in c for c in ms.gameboard)
     if(gameover):
         continue
-    #copy gameboard
-    # gb = [row[:] for row in ms.gameboard]
+    possibles = [[()]*cols for _ in range(rows)] 
     found=False
     #First, check for any obvious solutions (ie 1 with only 1 ?)
     for i in range(rows):
         for j in range(cols):
             try:
                 x = int(ms.gameboard[i][j])
-                # print(x)
+                
                 #check if # of ?+x neighbors == x
                 q = [] # queue of ?'s to flag
                 bombs = 0 # count of neighboring flagged bombs
@@ -40,21 +39,69 @@ while(not gameover):
                     for ij in q:
                         ms.test(ij[0],ij[1])
                         found=True
+                possibles[i][j] = (x-bombs,q)
                 
             except:
                 pass
+    if(found):
+        print("easy algorithm!")
+    #Possible neighbors
+    if not found:
+        for i in range(rows):
+            for j in range(cols):
+                if len(possibles[i][j]) > 0 and possibles[i][j][0]>0:
+                
+                    for ii in range(max(0,i-2),min(rows,i+3)):
+                        for jj in range(max(0,j-2),min(cols,j+3)):
+                            if (ii==i and jj==j):
+                                continue
+                            if len(possibles[ii][jj]) > 0 and possibles[ii][jj][0]>0:
+                
+                                if all(p in possibles[ii][jj][1] for p in possibles[i][j][1]):                                    
+                                    leftover = set(possibles[ii][jj][1])-set(possibles[i][j][1])
+                                    diff = possibles[ii][jj][0]-possibles[i][j][0]
+                                    
+                                    if(len(leftover)!=0):
+                                        print(possibles[i][j]," | ",possibles[ii][jj],end=" --> ")
+                                        print("possible algorithm: ",end='')
+                                        print(diff,leftover)
+                                        if(diff==0):
+                                            found=True
+                                            for p in leftover:
+                                                ms.test(p[0],p[1])
+                                            
+                                        elif(diff==len(leftover)):
+                                            found=True
+                                            for p in leftover:
+                                                ms.flag(p[0],p[1])
+                                            
+                                    
+                            if found:
+                                break
+                        if found:
+                            break
+                if found:
+                    break
+            if found:
+                break
+                            
+                                            
+                                    
+                                    
     #last resort, just randomly select
     gameover = not any("?" in c for c in ms.gameboard)
     if(gameover):
         continue
     if not found:
+        guesslist = []
+        for i in range(rows):
+            for j in range(cols):
+                if ms.gameboard[i][j] == "?":
+                    guesslist.append((i,j))
         
-        testi,testj=random.randint(0,9),random.randint(0,9)
-        
-        while(ms.gameboard[testi][testj] != "?"):
-            testi,testj=random.randint(0,9),random.randint(0,9)
-    
-        gameover = ms.test(testi,testj)
+        guess=random.choice(guesslist)
+        print("Guessing: out of",len(guesslist),": ",guess)
+        gameover = ms.test(guess[0],guess[1]) or not any("?" in c for c in ms.gameboard)
     
 won = not any("?" in c for c in ms.gameboard) and not any("o" in c for c in ms.gameboard)
 ms.print_gameboard()
